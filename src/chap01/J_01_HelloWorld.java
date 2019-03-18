1aa991b628b4de18e27b2b08d3ccc9319bcb1596
CONNOR KIM <connor@CONNORs-MacBook-Pro.local>
2019-03-18 01:13:39 -0700

    03월 18일 월요일

---
+++ src/chap01/J_01_HelloWorld.java
@@ -0,0 +1,8 @@
+package chap01;
+
+public class J_01_HelloWorld { psvm
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/chap01/J_02_Test.java
@@ -0,0 +1,7 @@
+package chap01;
+
+public class J_02_Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
